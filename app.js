5b4dc4d77a3eedd013fba4e961ceaf156baa3646
Matt <mbsabbanban@gmail.com>
2015-10-31 12:00:07 -0400

    Add: Added Panel Controller that controls Tabs and Panels in HTML

--- app.js
+++ app.js
@@ -5,12 +5,27 @@
 	    this.products = gems;
 	});
 	
+	//Adding a new Controller for the PANEL in the page
+	app.controller('PanelController', function(){
+	
+		this.tab = 1;
+		
+		this.selectTab = function(setTab){
+			this.tab = setTab;
+		};
+		
+		this.isSelected = function(checkTab){
+			return this.tab === checkTab; 
+		};
+		
+	});
+	
 	var gems = [
 	
 	{
 	  name: "Ramen",
 	  price: 2.95,
-	  description: '. . .',
+	  description: 'This is the Ramen',
 	  canPurchase: true,
 	  soldOut: false,
 	  images: [
@@ -24,7 +39,7 @@
 	{
 	  name: "Sunflowers",
 	  price: 3.95,
-	  description: '. . .',
+	  description: 'This is the Sunflower',
 	  canPurchase: true,
 	  soldOut: false,
 	  images: [
@@ -37,5 +52,5 @@
 	}
 	
 	];
-	
+
 })();
